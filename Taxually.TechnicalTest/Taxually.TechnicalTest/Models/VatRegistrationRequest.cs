03a8d3b86de8e13b6517f7d8b0e7c5fc5b4a78dd
János Orcsik <orcsikj@gmail.com>
2024-09-02 16:15:36 +0200

    Add default value to remove uninitialized warning during building

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Models/VatRegistrationRequest.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Models/VatRegistrationRequest.cs
@@ -2,7 +2,7 @@ namespace Taxually.TechnicalTest.Models;
 
 public class VatRegistrationRequest
 {
-    public string CompanyName { get; set; }
-    public string CompanyId { get; set; }
-    public string Country { get; set; }
+    public string CompanyName { get; set; } = string.Empty;
+    public string CompanyId { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
 }
